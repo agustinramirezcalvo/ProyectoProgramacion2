7051538abc42fc6d44380ca2d2890f359f7ca4e5
Agustín Ramírez Calvo <aramirezcalvo@udesa.edu.ar>
2022-04-08 17:06:04 -0300

    subiendo data.js

---
+++ db/data.js
@@ -0,0 +1,93 @@
+const data = {
+    usuario: {
+        usuario: 'Lola Biscay',
+        email: 'lolabiscay@gmail.com',
+        fechaDeNacimiento: 25/04/2003,
+        documento: 45000623,
+        //fotoDePerfil: 
+    },
+    producto: [
+        {
+            marca: 'Vans',
+            modelo: 'Old Skool',
+            color: 'Verde',
+            género: 'Streetwear',
+            año: '2022',
+            foto: ''
+        },
+        {
+            marca: 'Vans',
+            modelo: 'Ultra Range Exo',
+            color: 'Azul',
+            género: 'Streetwear',
+            año: '2021',
+            foto: ''
+        },
+        {
+            marca: 'DC Shoes',
+            modelo: 'Stag',
+            color: 'Negro',
+            género: 'Streetwear',
+            año: '2022',
+            foto: ''
+        },
+        {
+            marca: 'Nike',
+            modelo: 'Air zoom pegasus 38',
+            color: 'Blanco',
+            género: 'Running',
+            año: '2020',
+            foto: ''
+        },
+        {
+            marca: 'Nike',
+            modelo: 'Joyride',
+            color: 'Rojo',
+            género: 'Running',
+            año: '2021',
+            foto: ''
+        },
+        {
+            marca: 'Converse x Keith Haring',
+            modelo: 'Chuck 70',
+            color: 'Blanco y negro',
+            género: 'Streetwear',
+            año: '2022',
+            foto: ''
+        },
+        {
+            marca: 'Maison Margiela',
+            modelo: 'Botas Tabi',
+            color: 'Marrón',
+            género: 'Streetwear',
+            año: '2022',
+            foto: ''
+        },
+        {
+            marca: 'Rick Owens',
+            modelo: 'DRKSHDW',
+            color: 'Negro y blanco',
+            género: 'Streetwear',
+            año: '2022',
+            foto: ''
+        },
+        {
+            marca: 'Snow Peak y New Balance',
+            modelo: 'Niobium Concept 2',
+            color: 'Verde y negro',
+            género: 'Running',
+            año: '2021',
+            foto: ''
+        },
+        {
+            marca: 'New Balance by Kith',
+            modelo: '992',
+            color: 'Azul y blanco',
+            género: 'Streetwear',
+            año: '2020',
+            foto: ''
+        }
+    ]
+}
+
+module.exports = data
