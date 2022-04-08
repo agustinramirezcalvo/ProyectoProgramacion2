d23f43dbdddfdf2278d9ff9a0aca1364adb667c5
Francesco Scaglioni <francescoscaglionii@gmail.com>
2022-04-08 17:42:42 -0300

    Profile login, edit y register

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -4,7 +4,13 @@ const profileController = {
     },
     register : function(req, res){
         return res.render('register');
+    },
+    login : function(req, res) {
+        return res.render('login');
+    },
+    edit : function(req, res){
+        return res.render('profile-edit');
     }
-}
+};
 
 module.exports = profileController;
--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const profileController = require('../controllers/profileController');
+const profileController = require('../controllers/profileController')
 
 router.get('/profile', profileController.index);
 router.get('/profile/register', profileController.register);
+router.get('/profile/login',profileController.login);
+router.get('/profile/edit', profileController.edit);
 
 module.exports = router;
