fbe5273187af95b25274d39d770130ac1514a40d
Francesco Scaglioni <francescoscaglionii@gmail.com>
2022-04-14 18:28:05 -0300

    profile terminado
    controller corregido, routes corregidas, formularios ejs hechos y corregidos

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,16 +1,17 @@
+const data = require('../db/data');
 const profileController = {
     index : function (req, res){
-        return res.render('profile');
+        return res.render('profile', {usuario: data.usuario, productos: data.producto});
     },
     register : function(req, res){
-        return res.render('register');
+        return res.render('register', {usuario: data.usuario});
     },
     login : function(req, res) {
-        return res.render('login');
+        return res.render('login', {usuario: data.usuario});
     },
     edit : function(req, res){
-        return res.render('profile-edit');
-    }
+        return res.render('profile-edit', {usuario: data.usuario});
+    },
 };
 
 module.exports = profileController;
