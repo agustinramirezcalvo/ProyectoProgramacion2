997c1d2a2dcee4a71fc547fc9e9765430d12ba84
Agustín Ramírez Calvo <aramirezcalvo@udesa.edu.ar>
2022-04-15 12:42:49 -0300

    Correcciones

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,10 +3,10 @@ const data = require("../db/data")
 const productController = {
     detail: function(req,res){
         return res.render('product', {productos: data.producto,
-        comentarios: data.comentarios})
+        comentarios: data.comentarios, usuario: data.usuario})
     },
     add: function(req,res){
-        return res.render('product-add')
+        return res.render('product-add', {usuario: data.usuario})
     }
 }
 module.exports = productController
